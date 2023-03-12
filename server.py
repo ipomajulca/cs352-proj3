e11c757092e06835e79f8b044158c4bf9e1fafc7
Ivan <ivan@ivans-mbp.mynetworksettings.com>
2023-03-12 15:04:14 -0400

    adding step 5

--- server.py
+++ server.py
@@ -71,14 +71,12 @@ def sigint_handler(sig, frame):
 # Register the signal handler
 signal.signal(signal.SIGINT, sigint_handler)
 
-# TODO: put your application logic here!
 # Read login credentials for all users
 login_credentials = {}
 with open('passwords.txt') as passwords:
     for item in passwords:
         username, password = item.strip().split()
         login_credentials[username] = password
-        
 
 # Read secret data of all users
 secret_data = {}
@@ -87,6 +85,9 @@ with open('secrets.txt') as secrets:
         username, secret = item.strip().split()
         secret_data[username] = secret
         
+# Dictionary that maps tokens to usernames
+token_username = {}
+
 ### Loop to accept incoming HTTP connections and respond.
 while True:
     client, addr = sock.accept()
@@ -94,45 +95,13 @@ while True:
 
     # Let's pick the headers and entity body apart
     header_body = req.split('\r\n\r\n')
+
+    # Parse headers and entity body and perform various actions based on the user's input.
     headers = header_body[0]
     body = '' if len(header_body) == 1 else header_body[1]
     print_value('headers', headers)
     print_value('entity body', body)
-
-    # TODO: Put your application logic here!
-    # Parse headers and body and perform various actions based on the user's input.
-    # Dictionary that maps tokens to usernames
-    token_username = {}
-
-    # Parse headers and body and perform various actions based on the user's input.
-    if 'username' in body and 'password' in body:
-        # We have login credentials, so let's try to log the user in.
-        username = body.split('&')[0].split('=')[1]
-        password = body.split('&')[1].split('=')[1]
-        if username in login_credentials and login_credentials[username] == password:
-            # Successful login.
-            rand_val = random.getrandbits(64)
-            token_username[rand_val] = username
-            headers_to_send = 'Set-Cookie: token=%d\r\n' % rand_val
-            html_content_to_send = success_page + secret_data[username]
-        else:
-            # Bad credentials.
-            headers_to_send = ''
-            html_content_to_send = bad_creds_page
-    elif 'Cookie' in headers:
-        # We have a cookie, so let's check if it's valid.
-        cookie = headers.split('Cookie: ')[1].split('\r\n')[0]
-        token = cookie.split('=')[1]
-        if token in token_username:
-            # Valid cookie.
-            username = token_username[token]
-            html_content_to_send = success_page + secret_data[username]
-            headers_to_send = ''
-        else:
-            # Invalid cookie.
-            html_content_to_send = login_page
-            headers_to_send = ''
-    elif 'action' in body and body.split('=')[1] == 'logout':
+    if 'action' in body and body.split('=')[1] == 'logout':
         # Logout action.
         html_content_to_send = logout_page
         headers_to_send = 'Set-Cookie: token=\r\n'
@@ -140,12 +109,42 @@ while True:
         # New password action.
         html_content_to_send = new_password_page
         headers_to_send = ''
+        
+    elif 'Cookie' in headers:
+        # We have a cookie, so let's check if it's valid.
+        cookie = headers.split('Cookie: ')[1].split('\r\n')[0]
+        token = cookie.split('=')[1]
+
+        if(token):
+            token = int(token)
+            if token in token_username:
+                # Valid cookie.
+                username = token_username[token]
+                html_content_to_send = success_page + secret_data[username]
+                headers_to_send = ''
+            else:
+                # Invalid cookie.
+                html_content_to_send = login_page
+                headers_to_send = ''
+    elif 'username' in body and 'password' in body:
+        # We have login credentials, so let's try to log the user in.
+        username = body.split('&')[0].split('=')[1]
+        password = body.split('&')[1].split('=')[1]
+        if username in login_credentials and login_credentials[username] == password:
+            # Successful login.
+            rand_val = int(random.getrandbits(64))
+            token_username[rand_val] = username
+            headers_to_send = 'Set-Cookie: token=%d\r\n' % rand_val
+            html_content_to_send = success_page + secret_data[username]
+        else:
+            # Bad credentials.
+            headers_to_send = ''
+            html_content_to_send = bad_creds_page
+   
     else:
         # Default: Login page.
         html_content_to_send = login_page
         headers_to_send = ''
-
-
 
     # Construct and send the final response back to the client.
     response  = 'HTTP/1.1 200 OK\r\n'
